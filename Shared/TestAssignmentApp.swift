c184a3ba1824f205f482fd95cdc39bdabc1090cc
GurvinderxSingh <A00120851@canadore-stanford.com>
2021-07-25 12:55:32 -0700

    Initial Commit

---
+++ Shared/TestAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestAssignmentApp.swift
+//  Shared
+//
+//  Created by Gurvinder singh on 2021-07-25.
+//
+
+import SwiftUI
+
+@main
+struct TestAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
